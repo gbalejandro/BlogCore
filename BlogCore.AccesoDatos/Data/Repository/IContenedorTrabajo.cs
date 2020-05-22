6d65a325af454ad3d0da684efa31775685cc58e4
gbalejandro <alejandrobright@gmail.com>
2020-05-22 11:35:32 -0500

    Se crea el entorno de trabajo, agregando el modelo Categoria.

---
+++ BlogCore.AccesoDatos/Data/Repository/IContenedorTrabajo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BlogCore.AccesoDatos.Data.Repository
+{
+    public interface IContenedorTrabajo : IDisposable
+    {
+        ICategoriaRepository Categoria { get; }
+        //IArticuloRepository Articulo { get; }
+        //ISliderRepository Slider { get; }
+        //IUsuarioRepository Usuario { get; }
+
+        void Save();
+    }
+}

a527fce77d03d801f616db5ce2e0cee56d5bf1ac
gbalejandro <alejandrobright@gmail.com>
2020-05-26 15:53:11 -0500

    Se agrega el método Create al modelo Categoria.

--- BlogCore/Areas/Admin/Controllers/CategoriasController.cs
+++ BlogCore/Areas/Admin/Controllers/CategoriasController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BlogCore.AccesoDatos.Data.Repository;
+using BlogCore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogCore.Areas.Admin.Controllers
@@ -17,9 +18,29 @@ namespace BlogCore.Areas.Admin.Controllers
             _contenedorTrabajo = contenedorTrabajo;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _contenedorTrabajo.Categoria.Add(categoria);
+                _contenedorTrabajo.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
         }
 
         #region Llamadas a la API

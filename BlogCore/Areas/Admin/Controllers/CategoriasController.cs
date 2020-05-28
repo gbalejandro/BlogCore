de62a1017588cf0d711066d148647eaa2cf7f1c6
gbalejandro <alejandrobright@gmail.com>
2020-05-28 09:52:28 -0500

    Se crean los métodos Borrar y Editar para el modelo Categoría.

--- BlogCore/Areas/Admin/Controllers/CategoriasController.cs
+++ BlogCore/Areas/Admin/Controllers/CategoriasController.cs
@@ -43,11 +43,51 @@ namespace BlogCore.Areas.Admin.Controllers
             return View(categoria);
         }
 
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Categoria categoria = new Categoria();
+            categoria = _contenedorTrabajo.Categoria.Get(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _contenedorTrabajo.Categoria.Update(categoria);
+                _contenedorTrabajo.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+
         #region Llamadas a la API
         [HttpGet]
         public IActionResult GetAll()
         {
             return Json(new { data = _contenedorTrabajo.Categoria.GetAll() });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _contenedorTrabajo.Categoria.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error borrando categoria" });
+            }
+
+            _contenedorTrabajo.Categoria.Remove(objFromDb);
+            _contenedorTrabajo.Save();
+            return Json(new { success = true, message = "Categoría borrada correctamente" });
         }
         #endregion
     }
